1a6defacb927fa36821e050adff3a253dfd27fa7
Meone <xusong1313@qq.com>
2021-01-22 16:17:18 +0800

    Initial Commit

---
+++ Sources/CalendarControl.xcframework/ios-arm64/CalendarControl.framework/Headers/CalendarControl.h
@@ -0,0 +1,18 @@
+//
+//  CalendarControl.h
+//  CalendarControl
+//
+//  Created by 徐松 on 2021/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarControl.
+FOUNDATION_EXPORT double CalendarControlVersionNumber;
+
+//! Project version string for CalendarControl.
+FOUNDATION_EXPORT const unsigned char CalendarControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarControl/PublicHeader.h>
+
+
---
+++ Sources/CalendarControl.xcframework/ios-arm64_x86_64-simulator/CalendarControl.framework/Headers/CalendarControl.h
@@ -0,0 +1,27 @@
+//
+//  CalendarControl.h
+//  CalendarControl
+//
+//  Created by 徐松 on 2021/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarControl.
+FOUNDATION_EXPORT double CalendarControlVersionNumber;
+
+//! Project version string for CalendarControl.
+FOUNDATION_EXPORT const unsigned char CalendarControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarControl/PublicHeader.h>
+
+
+/*
+ xcodebuild archive \
+ -scheme CalendarControl \
+ -configuration Release \
+ -destination 'generic/platform=iOS' \
+ -archivePath './build/CalendarControl.framework-iphoneos.xcarchive' \
+ SKIP_INSTALL=NO \
+ BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+ */
